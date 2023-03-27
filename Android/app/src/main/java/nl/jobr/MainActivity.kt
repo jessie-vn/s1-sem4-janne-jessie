150da6ae3666b0c13c3b3389cc1e1d274be6432b
Seggelen,Janne J.M. van <janne.vanseggelen@student.fontys.nl>
2023-03-27 11:40:43 +0200

    Start saving answers in Array

--- Android/app/src/main/java/nl/jobr/MainActivity.kt
+++ Android/app/src/main/java/nl/jobr/MainActivity.kt
@@ -1,7 +1,12 @@
 package nl.jobr
 
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
 import android.view.View
+import android.webkit.WebView
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -34,8 +39,47 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+    private var idx: Int = 1;
+    private data class Question(val question1: String, val type1: Int, var answer1: String?, val question2: String, val type2: Int, var answer2: String?)
+    private val questions = arrayOf(
+        Question("What is your name?", InputType.TYPE_CLASS_TEXT, null,"What is your age?", InputType.TYPE_CLASS_NUMBER, null),
+        Question("What company are you currently working for?", InputType.TYPE_CLASS_TEXT, null,"What companies have you worked for?", InputType.TYPE_CLASS_TEXT, null),
+        Question("How long have you worked for your current company?", InputType.TYPE_CLASS_TEXT, null,"What skills do you have?", InputType.TYPE_CLASS_TEXT, null)
+    )
     fun continueSurvey(view: View) {
-        // Continue the survey
-        Toast.makeText(getBaseContext(), "Your resume has been updated", Toast.LENGTH_SHORT ).show();
+        for (i in idx until questions.size) {
+            updateQuestions(idx)
+            break
+        }
+        idx++
+
+        // Check if last question is already displayed
+        if (idx > questions.size) {
+            Toast.makeText(getBaseContext(), "Your resume has been updated", Toast.LENGTH_SHORT ).show();
+            idx = 0;
+            updateQuestions(idx)
+            idx = 1;
+        }
+    }
+    private fun updateQuestions(i: Int) {
+        val question1: TextView = findViewById(R.id.tvName)
+        val type1: EditText = findViewById(R.id.tbName)
+        val question2: TextView = findViewById(R.id.tvAge)
+        val type2: EditText = findViewById(R.id.nbAge)
+
+        // Clean Edit View
+        type1.text = null
+        type2.text = null
+
+        if (questions[i].answer1 != null) { type1.hint = questions[i].answer1 }
+        if (questions[i].answer2 != null) { type2.hint = questions[i].answer2 }
+
+        question1.text = questions[i].question1
+        type1.inputType = questions[i].type1
+        questions[i].answer1 = type1.text.toString()
+
+        question2.text = questions[i].question2
+        type2.inputType = questions[i].type2
+        questions[i].answer2 = type2.text.toString()
     }
 }
