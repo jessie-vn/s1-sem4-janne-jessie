198013c505a6879f49540f4b0f617031c0ef49d3
Nuenen,Jessie J. van <jessie.vannuenen@student.fontys.nl>
2023-03-30 08:33:09 +0000

    Main into home merge

--- Android/app/src/main/java/nl/jobr/ui/account/AccountFragment.kt
+++ Android/app/src/main/java/nl/jobr/ui/account/AccountFragment.kt
@@ -1,17 +1,18 @@
-package nl.jobr.ui.notifications
+package nl.jobr.ui.account
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import nl.jobr.databinding.FragmentNotificationsBinding
+import nl.jobr.databinding.FragmentAccountBinding
 
-class NotificationsFragment : Fragment() {
+class AccountFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentAccountBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +23,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val accountViewModel =
+            ViewModelProvider(this).get(AccountViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textAccount
+        accountViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
