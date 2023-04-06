c44ae7448cb9da4b4315fd2416a7d8f11969994d
Jessie van Nuenen <484943@student.fontys.nl>
2023-04-06 12:09:52 +0200

    Fixed paginate bug

--- Android/app/src/main/java/nl/jobr/ui/home/HomeFragment.kt
+++ Android/app/src/main/java/nl/jobr/ui/home/HomeFragment.kt
@@ -58,7 +58,7 @@ class HomeFragment : Fragment(), CardStackListener {
 
     override fun onCardSwiped(direction: Direction) {
         Log.d("CardStackView", "onCardSwiped: p = ${manager.topPosition}, d = $direction")
-        if (manager.topPosition == adapter.itemCount - 5) {
+        if (manager.topPosition == adapter.itemCount - 1) {
             paginate()
         }
     }
@@ -109,24 +109,18 @@ class HomeFragment : Fragment(), CardStackListener {
     private fun paginate() {
         val old = adapter.getCompanies()
         val new = old.plus(createCompanies())
-        val callback = SpotDiffCallback(old, new)
-        val result = DiffUtil.calculateDiff(callback)
-        adapter.setCompanies(new)
-        result.dispatchUpdatesTo(adapter)
+        if(new.isNotEmpty()){
+            val callback = SpotDiffCallback(old, new)
+            val result = DiffUtil.calculateDiff(callback)
+            adapter.setCompanies(new)
+            result.dispatchUpdatesTo(adapter)
+        }
     }
 
     private fun createCompanies(): List<Company> {
         val companies = ArrayList<Company>()
         companies.add(Company(name = "Yasaka Shrine", city = "Kyoto", url = "https://source.unsplash.com/Xq1ntWruZQI/600x800"))
         companies.add(Company(name = "Fushimi Inari Shrine", city = "Kyoto", url = "https://source.unsplash.com/NYyCqdBOKwc/600x800"))
-        companies.add(Company(name = "Bamboo Forest", city = "Kyoto", url = "https://source.unsplash.com/buF62ewDLcQ/600x800"))
-        companies.add(Company(name = "Brooklyn Bridge", city = "New York", url = "https://source.unsplash.com/THozNzxEP3g/600x800"))
-        companies.add(Company(name = "Empire State Building", city = "New York", url = "https://source.unsplash.com/USrZRcRS2Lw/600x800"))
-        companies.add(Company(name = "The statue of Liberty", city = "New York", url = "https://source.unsplash.com/PeFk7fzxTdk/600x800"))
-        companies.add(Company(name = "Louvre Museum", city = "Paris", url = "https://source.unsplash.com/LrMWHKqilUw/600x800"))
-        companies.add(Company(name = "Eiffel Tower", city = "Paris", url = "https://source.unsplash.com/HN-5Z6AmxrM/600x800"))
-        companies.add(Company(name = "Big Ben", city = "London", url = "https://source.unsplash.com/CdVAUADdqEc/600x800"))
-        companies.add(Company(name = "Great Wall of China", city = "China", url = "https://source.unsplash.com/AWh9C-QjhE4/600x800"))
         return companies
     }
 
