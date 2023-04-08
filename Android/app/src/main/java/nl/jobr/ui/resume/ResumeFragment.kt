cd64a983053ac3c847882cc8c2cadd8df032d82e
Seggelen,Janne J.M. van <janne.vanseggelen@student.fontys.nl>
2023-04-08 13:24:22 +0200

    Number above age slider not changing yet

--- Android/app/src/main/java/nl/jobr/ui/resume/ResumeFragment.kt
+++ Android/app/src/main/java/nl/jobr/ui/resume/ResumeFragment.kt
@@ -1,15 +1,17 @@
 package nl.jobr.ui.resume
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import nl.jobr.databinding.FragmentHomeBinding
 import nl.jobr.databinding.FragmentResumeBinding
+
 
 class ResumeFragment : Fragment() {
 
