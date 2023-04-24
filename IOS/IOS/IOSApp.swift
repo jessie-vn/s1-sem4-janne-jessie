d08a140e29a86b04acee2ac4c9d171557d707d5d
Janne van Seggelen <jannevanseggelen@Jannes-MacBook-Pro.local>
2023-04-24 09:32:04 +0200

    Start IOS

---
+++ IOS/IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by Janne van Seggelen on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
