be4cea6dae7917ed2d03798967a02dcf6205b87e
i486642 <i486642@fhict.nl>
2023-05-08 11:50:07 +0200

    Finish landing page + detailed page for products

--- IOS/IOS/Views/CartViewBlock.swift
+++ IOS/IOS/Views/CartViewBlock.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CartViewBlock: View {
     var title: String
-    var charts: [Chart]
+    var products: [Product]
     var body: some View {
         VStack(alignment: .leading) {
             Text(title)
@@ -19,36 +19,34 @@ struct CartViewBlock: View {
                 .padding(.bottom, 5)
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 20) {
-                    ForEach(charts) { chart in
-                        VStack {
-                            Text(chart.title)
-                                .fontWeight(.bold)
-                                .foregroundColor(Color(red: 0.62, green: 0.908, blue: 0.754))
-                                .font(.system(size: 15))
-                            Image(chart.content)
-                            /*.padding(10)
-                             .background(Color(red: 0.62, green: 0.908, blue: 0.754))
-                             .foregroundColor(.white)
-                             .cornerRadius(20)*/
-                                .resizable()
-                                .frame(width: 75, height: 75)
+                    ForEach(products) { product in
+                        NavigationLink {
+                            DetailsView(
+                                product: product
+                            )
+                        }
+                        label: {
+                            VStack {
+                                /*Text(product.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color(red: 0.62, green: 0.908, blue: 0.754))
+                                    .font(.system(size: 15))*/
+                                Image(product.image)
+                                /*.padding(10)
+                                 .background(Color(red: 0.62, green: 0.908, blue: 0.754))
+                                 .foregroundColor(.white)
+                                 .cornerRadius(20)*/
+                                    .resizable()
+                                    .frame(width: 75, height: 75)
+                            }
                         }
                     }
                 }
             }
             //.frame(maxWidth: 150)
             .padding(.bottom, 30)
-        }
-    }
-}
 
-class Chart: Identifiable {
-    var title: String
-    var content: String
-    
-    init(title: String, content: String) {
-        self.title = title
-        self.content = content
+        }
     }
 }
 
@@ -56,7 +54,7 @@ struct CartViewBlock_Previews: PreviewProvider {
     static var previews: some View {
         CartViewBlock(
             title: "",
-            charts: [Chart]()
+            products: [Product]()
         )
     }
 }
--- IOS/IOS/Views/ContentView.swift
+++ IOS/IOS/Views/ContentView.swift
@@ -10,7 +10,70 @@ import CodeScanner
 
 struct ContentView: View {
     @State var isPresentingScanner = false
-    @State var scannerCode: String = "Scan a code to get started."
+    @State var scannerCode: String = "Scan a code to get started"
+    
+    var hardcodedProducts: [Product] = [
+        Product(
+            image: "AH-Appelstroop",
+            title: "AH Appelstroop",
+            vegan: false,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            code: "1"
+        ),
+        Product(
+            image: "AH-Augurken",
+            title: "AH Augurken",
+            vegan: false,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            code: "2"
+        ),
+        Product(
+            image: "AH-Appelmouse",
+            title: "AH Appelmouse",
+            vegan: false,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            code: "3"
+        ),
+        Product(
+            image: "AH-KokosMelk",
+            title: "AH Kokosmelk",
+            vegan: true,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            code: "4"
+        ),
+        Product(
+            image: "AH-Ketchup",
+            title: "AH Ketchup",
+            vegan: true,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            code: "5"
+        ),
+        Product(
+            image: "AH-Peterselie",
+            title: "AH Peterselie",
+            vegan: false,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            code: "6"
+        )
+    ]
+    
+    var filteredProductList: [Product] =
+        [
+            Product(
+                image: "AH-KokosMelk",
+                title: "AH Kokosmelk",
+                vegan: true,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                code: "4"
+            ),
+            Product(
+                image: "AH-Ketchup",
+                title: "AH Ketchup",
+                vegan: true,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                code: "5"
+            )
+        ]
     
     var scannerSheet : some View {
         CodeScannerView(
@@ -25,106 +88,54 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack(spacing: 10) {
-            TitleBarView()
-            Divider()
-            VStack {
-                ScrollView {
-                    CartViewBlock(
-                        title: "Your previous scans:",
-                        charts:
-                        [
-                            Chart(
-                                title: "",
-                                content: "AH-Appelstroop"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "AH-Augurken"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "AH-Appelmouse"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "AH-KokosMelk"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "AH-Ketchup"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "AH-Peterselie"
-                            )
-
-                        ]
+        NavigationView {
+            VStack(spacing: 10) {
+                TitleBarView()
+                Divider()
+                VStack {
+                    ScrollView {
+                        CartViewBlock(
+                            title: "Your previous scans:",
+                            products: hardcodedProducts
+                        )
+                        CartViewInline(
+                            title: "Want to know more?",
+                            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                        )
+                        CartViewBlock(
+                            title: "These products might interest you:",
+                            products: filteredProductList
+                        )
+                    }
+                }
+                .padding(30)
+                
+                Spacer()
+                Divider()
+                
+                Text(scannerCode)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.62, green: 0.908, blue: 0.754))
+                Button("Scan a code") {
+                    self.isPresentingScanner = true
+                }
+    //            .buttonStyle(.borderedProminent)
+                .padding(9)
+                .foregroundColor(.white)
+                .background(
+                    RoundedRectangle(
+                        cornerRadius: 15,
+                        style: .continuous
                     )
-                    CartViewInline(
-                        title: "Want to know more?",
-                        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    )
-                    CartViewBlock(
-                        title: "These products might interest you:",
-                        charts:
-                        [
-                            Chart(
-                                title: "",
-                                content: "logo"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "logo"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "logo"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "logo"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "logo"
-                            ),
-                            Chart(
-                                title: "",
-                                content: "logo"
-                            )
-
-                        ]
-                    )
+                    .fill(Color(red: 0.62, green: 0.908, blue: 0.754))
+                )
+                .sheet(isPresented: $isPresentingScanner) {
+                    self.scannerSheet
                 }
             }
-            .padding(30)
-            
-            Spacer()
-            Divider()
-            
-            Text(scannerCode)
-                .fontWeight(.bold)
-                .foregroundColor(Color(red: 0.62, green: 0.908, blue: 0.754))
-            Button("Scan a code") {
-                self.isPresentingScanner = true
-            }
-//            .buttonStyle(.borderedProminent)
-            .padding(9)
-            .foregroundColor(.white)
-            .background(
-                RoundedRectangle(
-                    cornerRadius: 15,
-                    style: .continuous
-                )
-                .fill(Color(red: 0.62, green: 0.908, blue: 0.754))
-            )
-            .sheet(isPresented: $isPresentingScanner) {
-                self.scannerSheet
-            }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.white)
+        }
     }
 }
 
